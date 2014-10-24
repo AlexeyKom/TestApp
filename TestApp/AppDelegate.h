35a54e1c19baa7c6bc6f6fc5d33a12513dc39d26
Alexey <alex_kom_@mail.ru>
2014-10-24 11:13:54 +0400

    .ignore

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Alexey Komissarov on 24/10/14.
+//  Copyright (c) 2014 Kaspersky Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Alexey Komissarov on 24/10/14.
+//  Copyright (c) 2014 Kaspersky Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
